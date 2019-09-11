632fc9a502f439e53489f6057386354062d0f660
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-09-11 17:21:08 -0400

    updating constraints for VC 1

---
+++ Getting-Data-From-Online/Getting-Data-From-Online/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Getting-Data-From-Online
+//
+//  Created by Krystal Campbell on 9/11/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
