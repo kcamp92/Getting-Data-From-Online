93ce5199f924a8818b3285df23a032c2a9d6dd2c
Krystal Campbell <krystalcampbell@Krystals-MBP.fios-router.home>
2019-09-11 21:55:02 -0400

    add data to showModel file and imageHelper file

--- Getting-Data-From-Online/Models/ShowModel.swift
+++ Getting-Data-From-Online/Models/ShowModel.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+struct showWrapper: Codable {
+    let name: String
+    let rating: Rating
+    let image: Image
+}
+
+struct Rating: Codable {
+    let average: Double
+}
+
+struct Image: Codable {
+    let original: String
+}
+
+class ShowsAPIManager {
+    
+    private init() {}
+    static let shared = ShowsAPIManager()
+    
+    func getShows(completionHandler: @escaping (Result<showWrapper,Error>)-> Void){
+        
+//        var showURL = "http://api.tvmaze.com/search/shows?q=girls"
+//
+//        if let show = showTitle {
+//            let newStr = show.replacingOccurrences(of: " ", with: "-")
+//            showURL = "http://api.tvmaze.com/search/shows?q=\(newStr)"
+//        }
+//
+//
+        let urlString = "http://api.tvmaze.com/search/shows?q=girls"
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(ErrorHandling.badURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, _, error) in guard error == nil else {
+            completionHandler(.failure(ErrorHandling.noData))
+            return
+            }
+            guard let data = data else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            do {
+                let showData = try JSONDecoder().decode(showWrapper.self, from: data)
+                completionHandler(.success(showData))
+            } catch {
+                completionHandler(.failure(ErrorHandling.decodingError))
+            }
+            }.resume()
+    }
+}
+
--- Getting-Data-From-Online/URL Stuff/Image Helper.swift
+++ Getting-Data-From-Online/URL Stuff/Image Helper.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+class ImageHelper {
+    private init() {}
+    
+static let shared = ImageHelper()
+    
+    func getImage(urlString: String, completionHandler: @escaping(Result<UIImage, Error>) -> Void){
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(ErrorHandling.badURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url){(data, _, error) in guard error == nil else {
+            completionHandler(.failure(ErrorHandling.noData))
+            return
+            }
+            guard let data = data else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            guard let image = UIImage(data: data) else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            completionHandler(.success(image))
+        }
+        .resume()
+    }
+}
+
