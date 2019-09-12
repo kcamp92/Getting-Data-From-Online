c406237345e147512ea655cb769657e5a09cb9e4
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-09-12 19:01:03 -0400

    set up the rest of storyboard

---
+++ Getting-Data-From-Online/EpisodeFiles/EpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell.swift
+//  Getting-Data-From-Online
+//
+//  Created by Krystal Campbell on 9/12/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Getting-Data-From-Online/Getting-Data-From-Online/EpisodeDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EpisodeDetailViewController.swift
+//  Getting-Data-From-Online
+//
+//  Created by Krystal Campbell on 9/12/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
